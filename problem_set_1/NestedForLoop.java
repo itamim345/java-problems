f40502f4f6dea71810d1c22f424c9f59be9883a2
itamim345 <66485210+itamim345@users.noreply.github.com>
2021-07-11 15:58:34 +0530

    Add files via upload

---
+++ problem_set_1/NestedForLoop.java
@@ -0,0 +1,11 @@
+package problems_set_1;
+
+public class NestedForLoop {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+		 for(int j=1;j<=5;j++){
+		 System.out.println(i+" "+j);
+		 }
+		 }
+		 }
+}
